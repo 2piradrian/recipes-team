3fa939abbade61cbf7d9fce74e5fe53d7bdfe40e
Adrian Rodriguez <rodriguezcadr@gmail.com>
2023-02-27 15:08:05 -0300

    login scroll

--- client/flutter_app/lib/screens/login_screen.dart
+++ client/flutter_app/lib/screens/login_screen.dart
@@ -12,124 +12,124 @@ class LoginPage extends StatelessWidget {
     String _password = '';
 
     return Scaffold(
+      /* resizeToAvoidBottomInset: true, */
       backgroundColor: AppStyles.kSecondaryBackgroundColor,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Flexible(
-              child: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
                 decoration: const BoxDecoration(
                   image: DecorationImage(
-                    image: NetworkImage(
-                        "https://forum.quartertothree.com/uploads/default/original/2X/1/15a8eda9f81e0169698fd4587b4f4ca216d72f4f.gif"),
-                    fit: BoxFit.cover,
+                    image: AssetImage("assets/login_animation.gif"),
+                    fit: BoxFit.contain,
                   ),
                 ),
                 child: const SizedBox(
-                  height: 200,
+                  height: 220,
                   width: double.infinity,
                 ),
               ),
-            ),
-            SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const SizedBox(
-                      height: 50,
-                      width: double.infinity,
-                    ),
-                    Form(
-                      key: _formKey,
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'Correo Electrónico',
-                              prefixIcon: const Icon(Icons.mail),
-                              filled: true,
-                              fillColor: Colors.grey[200],
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                borderSide: BorderSide.none,
-                              ),
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Por favor, ingresa tu correo electrónico';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) {
-                              _email = value!;
-                            },
-                          ),
-                          const SizedBox(height: 20.0),
-                          TextFormField(
-                            decoration: InputDecoration(
-                              hintText: 'Contraseña',
-                              prefixIcon: const Icon(Icons.lock),
-                              filled: true,
-                              fillColor: Colors.grey[200],
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10.0),
-                                borderSide: BorderSide.none,
-                              ),
-                            ),
-                            obscureText: true,
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Por favor, ingresa tu contraseña';
-                              }
-                              return null;
-                            },
-                            onSaved: (value) {
-                              _password = value!;
-                            },
-                          ),
-                        ],
+              SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const SizedBox(
+                        height: 50,
+                        width: double.infinity,
                       ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _formKey.currentState!.save();
-                          // Aquí iría la lógica para verificar las credenciales
-                          // y acceder a la aplicación principal
-                        }
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.orangeAccent,
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'Correo Electrónico',
+                                prefixIcon: const Icon(Icons.mail),
+                                filled: true,
+                                fillColor: AppStyles.kBackgroundColor,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Por favor, ingresa tu correo electrónico';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _email = value!;
+                              },
+                            ),
+                            const SizedBox(height: 20.0),
+                            TextFormField(
+                              decoration: InputDecoration(
+                                hintText: 'Contraseña',
+                                prefixIcon: const Icon(Icons.lock),
+                                filled: true,
+                                fillColor: AppStyles.kBackgroundColor,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  borderSide: BorderSide.none,
+                                ),
+                              ),
+                              obscureText: true,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Por favor, ingresa tu contraseña';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _password = value!;
+                              },
+                            ),
+                          ],
                         ),
                       ),
-                      child: const Text(
-                        'Iniciar sesión',
-                        style: TextStyle(fontSize: 18.0),
+                      const SizedBox(height: 20.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _formKey.currentState!.save();
+                            // Aquí iría la lógica para verificar las credenciales
+                            // y acceder a la aplicación principal
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.orangeAccent,
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        child: const Text(
+                          'Iniciar sesión',
+                          style: TextStyle(fontSize: 18.0),
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 20.0),
-                    TextButton(
-                      onPressed: () {
-                        // Aquí iría la lógica para navegar a la pantalla de registro
-                      },
-                      child: Text(
-                        '¿No tienes una cuenta? Regístrate',
-                        style: TextStyle(color: Colors.grey[600]),
+                      const SizedBox(height: 20.0),
+                      TextButton(
+                        onPressed: () {
+                          // Aquí iría la lógica para navegar a la pantalla de registro
+                        },
+                        child: Text(
+                          '¿No tienes una cuenta? Regístrate',
+                          style: TextStyle(color: Colors.grey[600]),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- client/flutter_app/lib/style/app_styles.dart
+++ client/flutter_app/lib/style/app_styles.dart
@@ -6,22 +6,22 @@ class AppStyles {
   /* Colors */
   static const kMainColor = Color(0xffffab40);
   static const kSecondaryColor = Color(0xffd9491e);
-  static const kBackgroundColor = Color(0xffa6a6a6);
-  static const kSecondaryBackgroundColor = Color(0xfff2f2f2);
+  static const kBackgroundColor = Color(0xFFEEEEEE);
+  static const kSecondaryBackgroundColor = Color.fromARGB(255, 255, 255, 255);
   /* Spaces */
   final double kPaddingHorizontal = SizeConfig.blockSizeHorizontal! * 6;
   static const double kBorderRadius = 10.0;
   /* Fonts */
-  final kQuestialBold = GoogleFonts.questrial(
+  final kQuestialBold = GoogleFonts.montserrat(
     fontWeight: FontWeight.w700,
   );
-  final kQuestialSemibold = GoogleFonts.questrial(
+  final kQuestialSemibold = GoogleFonts.montserrat(
     fontWeight: FontWeight.w600,
   );
-  final kQuestialMedium = GoogleFonts.questrial(
+  final kQuestialMedium = GoogleFonts.montserrat(
     fontWeight: FontWeight.w500,
   );
-  final kQuestialRegular = GoogleFonts.questrial(
+  final kQuestialRegular = GoogleFonts.montserrat(
     fontWeight: FontWeight.w400,
   );
 }
